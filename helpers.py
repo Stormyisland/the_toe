16daa96b7c2f724c14842fd814d1a7870ba8adc7
Stormyisland <jacqueslavoie2015@gmail.com>
2024-06-04 14:14:47 -0700

    4 commit

--- helpers.py
+++ helpers.py
@@ -3,28 +3,24 @@ def draw_board(spots):
              f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
              f"|{spots[7]}|{spots[8]}|{spots[9]}|")
   print(board)
-
-
+  
 def check_turn(turn):
-    if turn % 2 == 0: return 'O'
-    else: return 'X'
+    return 'O' if turn % 2 == 0 else 'X'
 
-def check_for_win(dict):
-  # Handle Horizontal Cases
-        if (spots[1] == spots[2] == spots[3]) \
+def check_for_win(spots):
+    # Handle Horizontal Cases
+    if (spots[1] == spots[2] == spots[3]) \
     or (spots[4] == spots[5] == spots[6]) \
     or (spots[7] == spots[8] == spots[9]):
         return True
-  # Handle Vertical Cases
-        elif (spots[1] == spots[4] == spots[7]) \
+    # Handle Vertical Cases
+    elif (spots[1] == spots[4] == spots[7]) \
     or (spots[2] == spots[5] == spots[8]) \
     or (spots[3] == spots[6] == spots[9]):
-    return True
-  # Diagonal Cases
-        elif (spots[1] == spots[5] == spots[9]) \
-        or (spots[3] == spots[5] == spots[7]):
-    
-return True
-    
-else: 
-return False
+        return True
+    # Handle Diagonal Cases
+    elif (spots[1] == spots[5] == spots[9]) \
+    or (spots[3] == spots[5] == spots[7]):
+        return True
+    else:
+        return False
